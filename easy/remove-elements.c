e415fee05061072eac3691b013a8b95e4f934caf
krohank <132974033+krohank@users.noreply.github.com>
2023-12-16 10:20:49 +0530

    removing element using loops and if

---
+++ easy/remove-elements.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int i,k=0;
+
+    for(i=0;i<numsSize;i++){
+        if (nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+}
